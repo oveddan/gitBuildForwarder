37348a14e42953b6d3e0b942606a0ef1e62419de
Dan Oved <stangogh@gmail.com>
2012-07-03 18:13:19 -0400

    Wrote implementation for recursive forwarding of builds...for some reason tests are not working

--- test/gitBuildForwarder_tests.js
+++ test/gitBuildForwarder_tests.js
@@ -6,7 +6,7 @@ var should = require('chai').should()
 
 describe('gitBuildForwarder(config)', function(){
   it('should return a function', function(){
-    var result = gitBuildForwarder({});
+    var result = gitBuildForwarder({repositoryType : 'bitBucket'});
     result.should.be.a('function');
   });
   it('should require a config argument', function(){
@@ -101,51 +101,6 @@ describe('gitBuildForwarder(config)', function(){
       // should
       this.response.json.calledOnce.should.be.ok;
       this.response.json.calledWith(dummyResult).should.be.ok;
-    });
-  });
-
-  describe('forwardBuilds(token, repositoryAndBranches, callback)', function(){
-    it('should iterate through each repository and branch and forward it', function(){
-    });
-    it('should callback with aggregated list of results from forwarded builds', function(){
-    });
-  });
-
-  describe('forwardBuild(token, repositoryAndBranch, callback)', function(){
-    it("should callback with 'No build for branch' if forwarded url cannot be found", function(){
-
-    });
-    it('should invoke a request on forwarded url with token if can be found', function(){
-
-    });
-    it("should callback with 'forwarded' and response if can be found", function(){
-
-    });
-  });
-});
-
-describe('utils', function(){
-  describe('createParserForRepositoryType(repositoryType)', function(){
-    it('should return bitBucketPostParser if type is bitbucket', function(){
-    });
-    it('should throw error for unknown repository type', function(){
-    });
-  });
-});
-
-describe('bitBucketPostParser', function(){
-  describe('parseGitPost(req, callback)', function(){
-    it('should callback with error if req.body is null', function(){
-
-    });
-    it('should callback with error if req.body.commits is null or not an array', function(){
-
-    });
-    it('should callback with error if repository or repository.name is null', function(){
-
-    });
-    it('should callback with repository name and array of branches', function(){
-
     });
   });
 });
---
+++ test/lib_tests.js
@@ -0,0 +1,101 @@
+var should = require('chai').should()
+  , sinon = require('sinon')
+  , utils = require('../lib/utils')
+  , gitPostParsers = require('../lib/gitPostParsers');
+
+
+describe('utils', function(){
+  describe('createParserForRepositoryType(repositoryType)', function(){
+    it('should return bitBucketPostParser if type is bitBucket', function(){
+      var result = utils.createParserForRepositoryType('bitBucket');
+      result.should.equal(gitPostParsers.bitBucket);
+    });
+  });
+
+  describe('forwardBuilds(token, repositoryAndBranches, callback)', function(){
+    it('should iterate through each repository and branch and forward it', function(){
+      // setup
+      var originalForwardBuild = utils.forwardBuild;
+      utils.forwardBuild = sinon.spy();
+      var repositoryAndBranches = {
+        repository : "mainRepo",
+        branches : ['staging', 'master', 'dev']
+      };
+      // test
+      utils.forwardBuilds('1234', repositoryAndBranches, function(){});
+
+      // should
+      for(var i = 0; i < repositoryAndBranches.branches.length; i++){
+        console.log(utils.forwardBuild);
+        utils.forwardBuild.calledWith('1234', repositoryAndBranches.repository, repositoryAndBranches.branches[i]).should.be.ok;
+      }
+
+      // restore
+      utils.forwardBuild = originalForwardBuild;
+    });
+    it('should callback with aggregated list of results from forwarded builds', function(done){
+      var originalForwardBuild = utils.forwardBuild;
+      utils.forwardBuild = sinon.spy();
+      var repositoryAndBranches = {
+        repository : "mainRepo",
+        branches : ['staging', 'master', 'dev']
+      };
+      var results = ['this found something', 'could not find build', 'successfully invoked build'];
+
+      var expectedCalledBackData = {
+        repository : repositoryAndBranches.repository,
+        branchResults : {
+          staging : results[0],
+          master : results[1],
+          dev : results[2]
+        }
+      }
+
+      var callback = function(err, data){
+        data.should.eql(expectedCalledBackData);
+        done();
+      };
+
+      // test
+      utils.forwardBuilds('1234', repositoryAndBranches, function(){});
+
+      // get each callback and invoke it with result
+      utils.forwardBuild.firstCall.args[3](null, results[0]);
+      utils.forwardBuild.secondCall.args[3](null, results[1]);
+      utils.forwardBuild.thirdCall.args[3](null, results[2]);
+
+
+      // restore
+      utils.forwardBuild = originalForwardBuild;
+    });
+  });
+
+  describe('forwardBuild(token, repositoryAndBranch, callback)', function(){
+    it("should callback with 'No build for branch' if forwarded url cannot be found", function(){
+
+    });
+    it('should invoke a request on forwarded url with token if can be found', function(){
+
+    });
+    it("should callback with 'forwarded' and response if can be found", function(){
+
+    });
+  });
+});
+
+describe('bitBucketPostParser', function(){
+  describe('parseGitPost(req, callback)', function(){
+    it('should callback with error if req.body is null', function(){
+
+    });
+    it('should callback with error if req.body.commits is null or not an array', function(){
+
+    });
+    it('should callback with error if repository or repository.name is null', function(){
+
+    });
+    it('should callback with repository name and array of branches', function(){
+
+    });
+  });
+});
