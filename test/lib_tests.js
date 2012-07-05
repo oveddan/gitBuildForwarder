a0cfcb1ce414c8f50c3eb949edf6b152b1485af8
Dan Oved <stangogh@gmail.com>
2012-07-05 17:06:07 -0400

    Implemented bitBucket post parser

--- test/lib_tests.js
+++ test/lib_tests.js
@@ -15,16 +15,58 @@ describe('utils', function(){
 
 describe('bitBucketPostParser', function(){
   describe('parseGitPost(req, callback)', function(){
-    it('should callback with error if req.body is null', function(){
+    it('should callback with error if req.body is null', function(done){
+      gitPostParsers.bitBucket.parseGitPost({body : null}, function(err, result){
+        err.should.not.beNull;
+        done();
+      });
     });
-    it('should callback with error if req.body.commits is null or not an array', function(){
-
+    it('should callback with error if req.body.commits is not an array', function(done){
+      gitPostParsers.bitBucket.parseGitPost({body : { commits : {a : '5'} }}, function(err, result){
+        err.should.not.beNull;
+        done();
+      });
     });
-    it('should callback with error if repository or repository.name is null', function(){
-
+    it('should callback with error if repository or repository.name is null', function(done){
+      gitPostParsers.bitBucket.parseGitPost({
+        body :
+          { commits : [],
+          repository : {
+            name : null
+          }}
+        },
+        function(err, result){
+           err.should.not.beNull;
+           done();
+        });
     });
-    it('should callback with repository name and array of branches', function(){
+    it('should callback with repository name and array of branches', function(done){
 
+      // setup
+      var validRequest = {
+        body : {
+          commits : [
+            { branch : 'master'},
+            { branch : 'staging'},
+            { branch : 'production'}
+          ],
+          repository : {
+            name : 'your-company.site'
+          }
+        }
+      };
+
+      var expectedResult = {
+        repository : validRequest.body.repository.name,
+        branches: ["master", "staging", "production"]
+      };
+
+      // test
+      gitPostParsers.bitBucket.parseGitPost(validRequest, function(err, result){
+        // should
+        result.should.eql(expectedResult);
+        done();
+      });
     });
   });
 });
